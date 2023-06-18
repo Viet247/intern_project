499dc80745431899bd43bf25759a7cd58e83b238
vietnq2023 <vietnq@zigexn.vn>
2023-06-18 16:08:12 +0700

    First commit for update user

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -15,20 +15,33 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   end
 
+  def edit
+    self.resource = current_user
+  end
+
   def update
-    resource
-    if resource.update(account_update_params)
-      sign_in(resource, bypass: true)
-      redirect_to my_path
-    else
-      render :update
+    if params[:_method] == 'patch'
+      resource
+      if resource.update(account_update_params)
+        sign_in(resource, bypass: true)
+        redirect_to my_path
+      else
+        render :update
+      end
+    elsif params[:_method] == 'put'
+      if current_user.update(account_update_params)
+        flash.now[:alert] = "Update successfully!"
+        redirect_to my_path
+      else
+        render :edit
+      end
     end
   end
 
   private 
 
   def account_update_params
-    params.permit(:full_name, :password, :password_confirmation,:curriculum_vitate)
+    params.permit(:full_name, :password, :password_confirmation, :current_password, :curriculum_vitate)
   end
 end
 
